56f944fb8d6a9114347473eba79cb1046eb92414
Amit <amit1994@gmail.com>
2017-10-18 16:29:51 -0500

    svd w/ and without normalizer

--- code/fasttext_download.py
+++ code/fasttext_download.py
@@ -3,7 +3,8 @@ from gensim_download import pickle_rw
 
 if __name__ == "__main__":
     # Pickle lgs and languages lists
-    fasttext_lgs = ['de', 'en', 'ru','zh_yue','es']
-    fasttext_languages = ['German', 'English', 'Russian','Chinese','Spanish']
+    #fasttext_lgs = ['de', 'en', 'ru','zh_yue','es','fr','it','ja']
+    fasttext_lgs = ['fr','it','ja']
+    fasttext_languages = ['French','Italian','Japanese']
     pickle_rw(('fasttext_lgs', fasttext_lgs))
     pickle_rw(('fasttext_languages', fasttext_languages))
