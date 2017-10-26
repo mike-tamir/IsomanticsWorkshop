8588ebbb6199fc3999493e8ab23a280c75c414af
Michael Tamir <mntamir@gmail.com>
2017-10-25 20:09:21 -0700

    transition work to spectral decomposition phases for presentation

---
+++ isomatics_setup.sh
@@ -0,0 +1,21 @@
+conda update conda
+
+conda create -n isomantics python=3.6 anaconda
+
+source activate isomantics
+
+conda install keras
+conda install tensorflow
+conda install seaborn
+conda install pandas
+conda install matplotlib
+
+conda install pydrive
+conda install bottleneck
+
+pip install tools/
+
+
+
+#pip install pydrive
+#pip install bottleneck
