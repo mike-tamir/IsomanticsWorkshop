9672a636495190aa279a276764dfa7b4525d5e4d
MichaelTamir <mtamir@michaelmirsmbp2.attlocal.net>
2017-10-29 15:14:57 -0700

    archiving select unused code

--- tools/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="isomantics",
-    version="0.0.1",
-    packages=find_packages(),
-
-    install_requires=['scikit-learn', 'numpy', 'seaborn'],
-)
