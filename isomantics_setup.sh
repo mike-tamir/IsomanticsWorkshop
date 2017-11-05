c62fdf6cd608a557058502838ef724b32ac72f44
Amit <amit1994@gmail.com>
2017-11-05 09:48:11 -0800

    isomantics_setup.sh typo fixed

---
+++ isomantics_setup.sh
@@ -0,0 +1,13 @@
+conda update conda
+
+conda create -n isomantics python=3.6 anaconda
+
+source activate isomantics
+conda install keras
+conda install tensorflow
+conda install seaborn
+conda install pandas
+conda install matplotlib
+
+pip install pydrive
+pip install bottleneck
